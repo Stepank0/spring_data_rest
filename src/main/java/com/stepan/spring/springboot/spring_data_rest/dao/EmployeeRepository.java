3c2d5d73b414224b00e3037272a0375dfbcc0bc5
Stepan Koryagin <stepanswim@gmail.com>
2023-03-12 23:38:38 +0200

    spring-boot-starter-data-rest add

---
+++ src/main/java/com/stepan/spring/springboot/spring_data_rest/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.stepan.spring.springboot.spring_data_rest.dao;
+
+
+
+import com.stepan.spring.springboot.spring_data_rest.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    public List<Employee> findAllByName(String name);
+}
